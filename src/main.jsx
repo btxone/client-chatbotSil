1b6e8e8ba3bf709fe496a3b9d953d7e1ff4332b6
Ignacio Nelson <info@subwaydesign.com.ar>
2023-05-19 01:15:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,15 @@ import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('rckchat_root')).render(
+/*! For license information please see main.js.LICENSE.txt */
+const rootDiv = document.createElement('div');
+const rootId = 'rckcccc_root'
+rootDiv.id = rootId;
+
+// Agrega el elemento div al body del documento
+document.body.appendChild(rootDiv);
+
+ReactDOM.createRoot(document.getElementById(rootId)).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
